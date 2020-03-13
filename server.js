2cc7937f8ca20fc0bd25ffc2cfb54bee174eb168
pellesoderberg <pellesoederberg@gmail.com>
2020-03-13 19:23:07 +0100

    server setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+const PORT = process.env.PORT || 5000;
+
+console.log('Listening on port: ' + PORT);
+app.listen(PORT);
